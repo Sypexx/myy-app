11579256fc05ec9f6eb2e1e06328e297c645eac0
Sypexx <Sypexofficial@gmail.com>
2020-08-29 09:05:14 +0900

    Init push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <Header></Header>
+  );
+}
+
+export default App;
